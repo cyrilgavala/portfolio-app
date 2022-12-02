2e7e000bf484b65f367ab5fbea200dd669ec764a
Cyril Gavala <cyril.gavala@gmail.com>
2022-12-02 15:37:29 +0100

    Version 1.1.0:  - added new project  - added links to the repos in project's description

--- src/components/ProjectItem/component.tsx
+++ src/components/ProjectItem/component.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./styles.css";
 
+interface Link {
+  label: string;
+  url: string;
+}
+
 interface Props {
   className: string;
   title: string;
   description: string;
   skills: string;
   url: string;
+  links: Link[];
 }
+
 export const ProjectItem: React.FC<Props> = (props) => {
   return (
     <div className="project-wrapper">
@@ -23,12 +30,25 @@ export const ProjectItem: React.FC<Props> = (props) => {
           <p>{props.description}</p>
         </div>
         <div>
-          <h3>What I practised?</h3>
+          <h3>What did I practise?</h3>
           <p>{props.skills}</p>
         </div>
         <a href={props.url} target="_blank" rel="noreferrer">
           Here you can check it out
         </a>
+        <div>
+          <h3>Here are the links to the source code:</h3>
+          <ul>
+            {props.links.map((link) => (
+              <li key={link.label}>
+                ›
+                <a href={link.url} target="_blank" rel="noreferrer">
+                  {link.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/pages/Projects/component.tsx
+++ src/pages/Projects/component.tsx
@@ -16,6 +16,11 @@ export const Projects: React.FC = () => {
         description="Full-stack project of three applications. Web-scrap one 'steals' data from https://korona.gov.sk and stores them into cloud Mongo database. Simple Node/Express API serves to communicate between ReactJS application and the database."
         skills="JavaScript, ReactJS, NodeJS, Express, web-scrap, MongoDB"
         url="https://covid-19-webapp.vercel.app"
+        links={[
+          { label: "UI", url: "https://github.com/cyrilgavala/covid-19-webapp" },
+          { label: "API", url: "https://github.com/cyrilgavala/covid-19-api" },
+          { label: "Web-scrap tool", url: "https://github.com/cyrilgavala/covid-19-web-scrap" }
+        ]}
       />
       <ProjectItem
         className="reservations"
@@ -23,6 +28,10 @@ export const Projects: React.FC = () => {
         description="Full-stack project of two applications. Simple backend Spring boot application with JWT authentication and authorization using Spring security and cloud MongoDB database. ReactJS frontend app for simple UI."
         skills="JavaScript, ReactJS, Spring boot, Spring security, JWT, MongoDB"
         url="https://cg-reservations-ui.vercel.app"
+        links={[
+          { label: "UI", url: "https://github.com/cyrilgavala/reservations-ui" },
+          { label: "API", url: "https://github.com/cyrilgavala/reservations-api" }
+        ]}
       />
       <ProjectItem
         className="wedding-gallery"
@@ -30,6 +39,15 @@ export const Projects: React.FC = () => {
         description="ReactJS photo gallery from our wedding using Dropbox API."
         skills="JavaScript, ReactJS, DropboxAPI"
         url="https://wedding-gallery.vercel.app"
+        links={[{ label: "UI", url: "https://github.com/cyrilgavala/cyril-vierka-wedding-gallery" }]}
+      />
+      <ProjectItem
+        className="noise-player"
+        title="White noise player"
+        description="Simple web page built with React to serve as white noise player."
+        skills="JavaScript, ReactJS, React hooks"
+        url="https://white-noise-player.vercel.app"
+        links={[{ label: "UI", url: "https://github.com/cyrilgavala/white-noise-player" }]}
       />
       <ProjectItem
         className="email-sender"
@@ -37,6 +55,7 @@ export const Projects: React.FC = () => {
         description="ReactJS application containing integrated Gmail login and interface for sending bulk emails."
         skills="JavaScript, ReactJS, GoogleAPI"
         url="https://email-sender-webapp.vercel.app"
+        links={[{ label: "UI", url: "https://github.com/cyrilgavala/email-sender-webapp" }]}
       />
       <ProjectItem
         className="workouts"
@@ -44,6 +63,10 @@ export const Projects: React.FC = () => {
         description="Another full-stack project having Node/Express API for client-server communication and ReactJS application for saving a progress of various workout challenges and their visualisation."
         skills="JavaScript, ReactJS, NodeJS, Express, MongoDB"
         url="https://workout-challenges-webapp.vercel.app"
+        links={[
+          { label: "UI", url: "https://github.com/cyrilgavala/workout-challenges-webapp" },
+          { label: "API", url: "https://github.com/cyrilgavala/workout-challenges-api" }
+        ]}
       />
     </div>
   );
